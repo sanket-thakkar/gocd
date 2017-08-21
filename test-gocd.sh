f5c761ce0388af4edd50b3618a97d7c8978ef388
sanket-thakkar <sankett1810@gmail.com>
2017-08-21 16:22:44 +0530

    Adding script
    Adding script to read environment variables

---
+++ test-gocd.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#Script to print env variables
+
+echo "Printing env variables..."
+echo $GO_SERVER_URL
+echo $GO_TRIGGER_USER
+echo $GO_PIPELINE_NAME
+echo $GO_PIPELINE_COUNTER
+echo $GO_PIPELINE_LABEL
+echo $GO_STAGE_NAME
+echo $GO_STAGE_COUNTER
+echo $GO_JOB_NAME
+echo $GO_REVISION
+echo $GO_TO_REVISION
+echo $GO_FROM_REVISION
+echo "End printing env variables."
